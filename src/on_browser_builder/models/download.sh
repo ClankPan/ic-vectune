12f5d3ed85e720e99a8fddb64039b076732abed3
clankpan <clankpan26cm@gmail.com>
2024-08-08 13:56:11 +0900

    fix model download script to allocate a file to its directory

--- src/on_browser_builder/models/download.sh
+++ src/on_browser_builder/models/download.sh
@@ -1,3 +1,5 @@
-curl -L -o ./src/on_browser_builder/models/model.safetensors 'https://huggingface.co/intfloat/e5-small-v2/resolve/main/model.safetensors'
-curl -L -o ./src/on_browser_builder/models/config.json 'https://huggingface.co/intfloat/e5-small-v2/resolve/main/config.json'
-curl -L -o ./src/on_browser_builder/models/tokenizer.json 'https://huggingface.co/intfloat/e5-small-v2/resolve/main/tokenizer.json'
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+curl -L -o "$SCRIPT_DIR/model.safetensors" 'https://huggingface.co/intfloat/e5-small-v2/resolve/main/model.safetensors'
+curl -L -o "$SCRIPT_DIR/config.json" 'https://huggingface.co/intfloat/e5-small-v2/resolve/main/config.json'
+curl -L -o "$SCRIPT_DIR/tokenizer.json" 'https://huggingface.co/intfloat/e5-small-v2/resolve/main/tokenizer.json'
